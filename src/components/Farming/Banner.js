5b2221c8a66f860a6f9b0364ddf33499fb3cd7ff
vrthedev <vishnurao@outlook.com>
2022-08-12 02:06:01 +0900

    videeo

--- src/components/Farming/Banner.js
+++ src/components/Farming/Banner.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Video from "react-responsive-video";
+
 import "./Farming.css";
 
 const initData = {
@@ -20,31 +22,39 @@ class Banner extends Component {
 
   render() {
     return (
-      <section
-        className="customBanner" id="banner"
-      >
-        <div className="container" style={{textAlign: "center"}}>
-          <a className="navbar-brand" href="/">
-            <img
-              className="navbar-brand-sticky mlogo"
-              src="img/logo_white.svg"
-              alt="sticky brand-logo"
-              style={{zIndex: 2001}}
-            />
-          </a>
-          <div className="justify-content-center">
-            <div className="intro text-center customFont">
-              <h1 className="mb-1 text-blue" style={{ marginTop: "7vh", fontFamily: "inherit" }}>
-                MASTER NODES
-              </h1>
-              <h2 className="mt-1 mb-0" style={{fontFamily: "inherit"}}>FOR PASSIVE INCOME</h2>
-              <h3 className="mt-2 mb-0" style={{fontFamily: "inherit", fontWeight: "normal" }}>
-                Buy your TurkNodes & earn <br /> through the blockchain
-              </h3>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Video mp4={`img/turknode.mp4`} />
+
+      // <section className="customBanner" id="banner">
+      //   <div className="container" style={{ textAlign: "center" }}>
+      //     <a className="navbar-brand" href="/">
+      //       <img
+      //         className="navbar-brand-sticky mlogo"
+      //         src="img/logo_white.svg"
+      //         alt="sticky brand-logo"
+      //         style={{ zIndex: 2001 }}
+      //       />
+      //     </a>
+      //     <div className="justify-content-center">
+      //       <div className="intro text-center customFont">
+      //         <h1
+      //           className="mb-1 text-blue"
+      //           style={{ marginTop: "7vh", fontFamily: "inherit" }}
+      //         >
+      //           MASTER NODES
+      //         </h1>
+      //         <h2 className="mt-1 mb-0" style={{ fontFamily: "inherit" }}>
+      //           FOR PASSIVE INCOME
+      //         </h2>
+      //         <h3
+      //           className="mt-2 mb-0"
+      //           style={{ fontFamily: "inherit", fontWeight: "normal" }}
+      //         >
+      //           Buy your TurkNodes & earn <br /> through the blockchain
+      //         </h3>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </section>
     );
   }
 }
