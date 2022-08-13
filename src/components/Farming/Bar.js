fff578b5209281e0c466cce75198a2ed4accf1b1
vrthedev <vishnurao@outlook.com>
2022-08-13 17:17:35 +0900

    apr image

--- src/components/Farming/Bar.js
+++ src/components/Farming/Bar.js
@@ -25,23 +25,23 @@ class Bar extends Component {
         </div>
         <div
           className="row justify-content-center"
-          style={{ width: "60vw", margin: "auto" }}
+          style={{ width: "90%", maxWidth: "700px", margin: "auto" }}
         >
           <div className="col-4">
             <div className="text-center relative_">
-              <img src="/img/chart-01.png" width={"50%"} />
+              <img src="/img/chart-01.png" className="apr_img" />
               <h6 className="mt-2 mb-3 text-red customDate">1st YEAR</h6>
             </div>
           </div>
           <div className="col-4">
             <div className="text-center relative_">
-              <img src="/img/chart-02.png" width={"50%"} />
+              <img src="/img/chart-02.png" className="apr_img" />
               <h6 className="mt-2 mb-3 text-red customDate">2nd YEAR</h6>
             </div>
           </div>
           <div className="col-4">
             <div className="text-center">
-              <img src="/img/chart-03.png" width={"50%"} />
+              <img src="/img/chart-03.png" className="apr_img" />
               <h6 className="mt-2 mb-3 text-red customDate">3rd YEAR</h6>
             </div>
           </div>
